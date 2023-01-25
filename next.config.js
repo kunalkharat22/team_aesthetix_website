cc3682af772b7d39c78913cfa5b9a7c07f305519
kunalkharat22 <kunalkharat22@users.noreply.github.com>
2023-01-25 14:33:52 +0530

    2nd chnages

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ const nextConfig = {
   env: {
     REACT_APP_INSTAGRAM_ACCESS_TOKEN : process.env.REACT_APP_INSTAGRAM_ACCESS_TOKEN,
     REACT_APP_INSTAGRAM_APP_SECRET: process.env.REACT_APP_INSTAGRAM_APP_SECRET,
-  }
+  },
+  swcMinify: false // it should be false by default 
 }
 
 module.exports = nextConfig
