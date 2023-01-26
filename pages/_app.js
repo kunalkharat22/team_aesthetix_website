1f39e6d86384b5b85e209e6a8be5aecd9da29a64
kunalkharat22 <kunalkharat22@users.noreply.github.com>
2023-01-26 15:21:46 +0530

    second comit

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,11 @@ import {Layout} from '../components'
 import { StateContext } from '../context/StateContext'
 import { Toaster } from 'react-hot-toast'
 import Router from "next/router";
-import nProgress from "nprogress";
+// import nProgress from "nprogress";
 
-Router.events.on("routeChangeStart", (nProgress.start));
-Router.events.on("routeChangeError", nProgress.done);
-Router.events.on("routeChangeComplete", nProgress.done);
+// Router.events.on("routeChangeStart", (nProgress.start));
+// Router.events.on("routeChangeError", nProgress.done);
+// Router.events.on("routeChangeComplete", nProgress.done);
 
 
 function MyApp({ Component, pageProps }) {
